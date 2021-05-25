f9e8cb9be79b363d9ecb59247ec1bac20d856a98
MSzostaq <maciek.szostak55@gmail.com>
2021-05-25 13:39:32 +0200

    changed colors and added card component

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Button from "components/Button";
 
 const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.blue};
+  background-color: ${({ theme }) => theme.colors.background};
   position: relative;
   height: 100%;
 `;
@@ -14,11 +14,34 @@ const ViewWrapper = styled.div`
   justify-content: center;
 `;
 
+const Card = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  border-radius: 12px;
+  box-shadow: 0 0 2px 0 ${({ theme }) => theme.colors.darkPurple};
+  margin-top: 128px;
+  width: 80vw;
+  height: 80vh;
+
+  @media (min-width: 800px) {
+    width: 60vw;
+  }
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.darkGrey};
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  font-family: sans-serif;
+  font-weight: bold;
+  margin: 12px;
+`;
+
 const Dashboard = () => {
   return (
     <Wrapper>
       <ViewWrapper>
-        <Button>Dashboard</Button>
+        <Card>
+          <Title>TODO</Title>
+        </Card>
       </ViewWrapper>
     </Wrapper>
   );
