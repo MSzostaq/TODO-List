a41c3e57775092a876ec3d31c50325dca660dfe8
MSzostaq <maciek.szostak55@gmail.com>
2021-05-25 14:25:19 +0200

    removed background-color and changed text color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Button = styled(motion.button)`
-  background-color: ${({ theme }) => theme.colors.lightGrey};
   border-radius: 8px;
-  color: ${({ theme }) => theme.colors.darkGrey};
+  color: ${({ theme }) => theme.colors.white};
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: ${({ theme }) => theme.fontSize.m};
   font-weight: bold;
+  padding: 4px;
 `;
 
 export default Button;
