1764419164dc97ad18b0fea47fc436cd9f012138
MSzostaq <maciek.szostak55@gmail.com>
2021-06-06 15:48:22 +0200

    actions

--- src/actions/todosActions.js
+++ src/actions/todosActions.js
@@ -1,4 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const ADD_TODO = "ADD_TODO";
+export function addTodo({
+  id = uuidv4(),
+  todoListId = null,
+  name = "",
+  isDone = false,
+}) {
+  return {
+    type: ADD_TODO,
+    payload: { id, todoListId, name, isDone },
+  };
+}
+
 export const REMOVE_TODO = "REMOVE_TODO";
 export const RENAME_TODO = "RENAME_TODO";
 export const UPDATE_TODO_STATUS = "UPDATE_TODO_STATUS";
