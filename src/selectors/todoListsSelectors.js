826af59b0411e589e2ecf26974941e610f366daf
MSzostaq <maciek.szostak55@gmail.com>
2021-06-26 12:37:07 +0200

    added selectors with tests

--- src/selectors/todoListsSelectors.js
+++ src/selectors/todoListsSelectors.js
@@ -1,3 +1,9 @@
+import { createSelector } from "reselect";
+
 export const getTodoListsById = (state) => state.entities.todoLists;
+
+export const getTodoLists = createSelector(getTodoListsById, (items) =>
+  Object.keys(items).map((id) => items[id])
+);
 
 export const getTodoListById = (state, id) => state.entities.todoLists[id];
