b790d67466b5871d9c450e49f09a3941ece3e6cf
MSzostaq <maciek.szostak55@gmail.com>
2021-07-03 13:00:35 +0200

    changed Tick width and height

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -6,12 +6,17 @@ const Box = styled.div`
   border: 2px solid ${({ theme }) => theme.colors.buttonGrey};
   border-radius: 4px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 24px;
   height: 24px;
 `;
 
 const Tick = styled(Icon)`
   color: ${({ theme }) => theme.colors.icons};
+  width: 16px;
+  height: 16px;
 `;
 
 const Checkbox = ({ className, value, onChange }) => {
