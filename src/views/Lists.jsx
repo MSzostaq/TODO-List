bbf00d18e88dd8b0c70ea33355c264ee8f7e6ad3
MSzostaq <maciek.szostak55@gmail.com>
2021-09-05 14:31:15 +0200

    added Header with Heading to Lists view

--- src/views/Lists.jsx
+++ src/views/Lists.jsx
@@ -2,17 +2,52 @@ import React from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { addList } from "actions/todoListsActions";
 import { getTodoLists } from "selectors";
 
 const View = styled.div`
   background-color: ${({ theme }) => theme.colors.background};
+  padding-top: 52px;
   position: relative;
   height: 100%;
 `;
 
+const Header = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.buttonGrey};
+  display: flex;
+  align-items: center;
+  padding-left: 12px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 52px;
+`;
+
+const Heading = styled.p`
+  color: ${({ theme }) => theme.colors.darkGrey};
+  font-size: ${({ theme }) => theme.fontSize.xxl};
+  font-weight: bold;
+  line-height: 52px;
+  height: 52px;
+`;
+
+const NewListButton = styled.div`
+  background-color: ${({ theme }) => theme.colors.succes};
+  border-radius: 8px;
+  color: ${({ theme }) => theme.colors.white};
+  cursor: pointer;
+  font-size: ${({ theme }) => theme.fontSize.m};
+  font-weight: bold;
+  line-height: 32px;
+  margin-left: 12px;
+  padding: 0 8px;
+  height: 32px;
+`;
+
 const Cards = styled.div`
   display: flex;
-  padding: 16px;
+  padding: 8px 12px;
 `;
 
 const ListCard = styled(NavLink)`
@@ -27,9 +62,17 @@ const ListCard = styled(NavLink)`
   padding: 8px;
 `;
 
-const Lists = ({ lists }) => {
+const Lists = ({ dispatch, lists }) => {
+  function onNewListButtonClick() {
+    dispatch(addList());
+  }
+
   return (
     <View>
+      <Header>
+        <Heading>Lists</Heading>
+        <NewListButton onClick={onNewListButtonClick}>New list</NewListButton>
+      </Header>
       <Cards>
         {lists.map((list) => (
           <ListCard key={list.id} to={`/list/${list.id}`}>
