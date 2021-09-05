aa5327d95a81e896efb81d0198beba7f03c9d57c
MSzostaq <maciek.szostak55@gmail.com>
2021-09-05 14:30:54 +0200

    added todoListsSagas to rootSaga func

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
+import todoListsSagas from "sagas/todoListsSagas";
 import notificationsSagas from "sagas/notificationsSagas";
 
 export default function* rootSaga() {
-  yield all([...notificationsSagas]);
+  yield all([...todoListsSagas, ...notificationsSagas]);
 }
