88c3abd0a1a448ea12aa7822e97429a1910e37e7
MSzostaq <maciek.szostak55@gmail.com>
2021-09-05 14:29:15 +0200

    added addList action creator

--- src/actions/todoListsActions.js
+++ src/actions/todoListsActions.js
@@ -1,4 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const ADD_LIST = "ADD_LIST";
+export const addList = ({ id = uuidv4(), name = "", items = [] } = {}) => ({
+  type: ADD_LIST,
+  payload: { id, name, items },
+});
+export const ADD_LIST_OK = "ADD_LIST_OK";
+export const ADD_LIST_ERROR = "ADD_LIST_ERROR";
 export const REMOVE_LIST = "REMOVE_LIST";
 export const RENAME_LIST = "RENAME_LIST";
 export const UPDATE_LIST_ITEMS_ORDER = "UPDATE_LIST_ITEMS_ORDER";
