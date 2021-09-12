4a604e13987a2fcb1fea065804c98f987c9b470a
MSzostaq <maciek.szostak55@gmail.com>
2021-09-12 14:35:38 +0200

    added onRemoveTodoList saga

--- src/sagas/todoListsSagas.js
+++ src/sagas/todoListsSagas.js
@@ -4,6 +4,9 @@ import {
   ADD_LIST,
   ADD_LIST_OK,
   ADD_LIST_ERROR,
+  REMOVE_LIST,
+  REMOVE_LIST_OK,
+  REMOVE_LIST_ERROR,
 } from "actions/todoListsActions";
 
 function* onAddTodoList(action) {
@@ -30,4 +33,29 @@ function* onAddTodoList(action) {
   }
 }
 
-export default [takeEvery(ADD_LIST, onAddTodoList)];
+function* onRemoveTodoList(action) {
+  try {
+    // todo: call API endpoint
+    yield put({ type: REMOVE_LIST_OK, payload: { id: action.payload.id } });
+    yield put(
+      addNotification({
+        title: "List removed",
+        message: `Id: ${action.payload.id}`,
+      })
+    );
+  } catch (err) {
+    yield put({ type: REMOVE_LIST_ERROR, payload: err });
+    yield put(
+      addNotification({
+        type: "error",
+        title: "Error",
+        message: `List not removed.`,
+      })
+    );
+  }
+}
+
+export default [
+  takeEvery(ADD_LIST, onAddTodoList),
+  takeEvery(REMOVE_LIST, onRemoveTodoList),
+];
