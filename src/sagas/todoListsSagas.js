9d7864e7a8c5072646eae2bbd394121c4a38491f
MSzostaq <maciek.szostak55@gmail.com>
2021-10-18 16:20:33 +0200

    added REMOVE_NOTIFICATION

--- src/sagas/todoListsSagas.js
+++ src/sagas/todoListsSagas.js
@@ -1,5 +1,9 @@
-import { call, put, takeEvery } from "redux-saga/effects";
-import { addNotification } from "actions/notificationsActions";
+import { call, delay, put, race, take, takeEvery } from "redux-saga/effects";
+import {
+  addNotification,
+  CLOSE_NOTIFICATION,
+  REMOVE_NOTIFICATION,
+} from "actions/notificationsActions";
 import {
   ADD_LIST,
   ADD_LIST_OK,
@@ -10,7 +14,13 @@ import {
 } from "actions/todoListsActions";
 
 function* onAddTodoList(action) {
+  const { id, lifespan } = action.payload;
   try {
+    yield race([
+      delay(lifespan, true),
+      take((a) => a.type === CLOSE_NOTIFICATION && a.payload.id === id),
+    ]);
+    yield put({ type: REMOVE_NOTIFICATION, payload: { id } });
     // todo: call API endpoint
     const res = { data: action.payload };
     yield put({ type: ADD_LIST_OK, payload: res.data });
@@ -34,7 +44,13 @@ function* onAddTodoList(action) {
 }
 
 function* onRemoveTodoList(action) {
+  const { id, lifespan } = action.payload;
   try {
+    yield race([
+      delay(lifespan, true),
+      take((a) => a.type === CLOSE_NOTIFICATION && a.payload.id === id),
+    ]);
+    yield put({ type: REMOVE_NOTIFICATION, payload: { id } });
     // todo: call API endpoint
     yield put({ type: REMOVE_LIST_OK, payload: { id: action.payload.id } });
     yield put(
